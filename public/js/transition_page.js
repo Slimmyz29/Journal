0bf878159c0875ab44cc63a1d7c6ef2913b35c60
Slimmyz29 <105140312+Slimmyz29@users.noreply.github.com>
2023-03-16 17:13:39 +0100

    Add files via upload

--- public/js/transition_page.js
+++ public/js/transition_page.js
@@ -13,6 +13,9 @@ content_page1 = [
     <h3><strong>15 Mars XXX</strong></h3>
     <p>Le capitaine m'a tester c'est bien ce que je pensais j'ai une seconde personalité elle se developpe a chaque fois que je suis en danger ou alors que mes amis le sont!il compte sur moi je ne vais pas le decevoir.
     <button onclick="PageSuivante(2)">Page Précedente</button></div>
+    <h3><strong>16 Mars XXX</strong></h3>
+    <p> Aujourd'hui c'etait mon jours de repos j'en est donc profiter pour aller dans les cartier chaud rendre visite a mon pere...il est un peu comme moi mais il est beaucoup plus courageux,je lui est posé des question sur ma double personalité,il m'a dit qu'il ne voulait pas m'en parler et que je devais trouver tous seul.</p>
+    <img src="public/img/photo.jpg" style="width: auto;height: 30%;" alt="">
     `,
     `
     `,
@@ -22,7 +25,7 @@ content_page2 = [
     `
     <h3><strong>13 Mars XXX</strong></h3>
     <p> Aujourd’hui,je suis partit en entrainement avec le BDE,nous somme monter dans les hauteur du camps d'entrainement et nous nous somme entrainer a l'épée,mais ne vous inquitez pas le petit Azrael n'est plus le meme je suis devenue bien plus fort qu'avant j'ai d'ailleur battut Werner en combat.</p>
-    <h3><strong>14 Février XXX</strong></h3>
+    <h3><strong>14 Mars XXX</strong></h3>
     <p>Un homme a ouvert la porte! j'ecris cela maitenant car si un titan rentre c'est la mort assuré pour moi...Papi si je meurt je t'aime...Je suis encore en vie!!!! nous avons fait sortir le titant et avons fait des patrouille,mais malheuresement Traffi c'est fait attaqué part un traitre du battaillon je ne sais pas comment mais j'ai tout oublier je me souvien juste avoir defendu traffi...</p>
     <button onclick="PageSuivante(3)">Page Suivante</button></div>
     `,
